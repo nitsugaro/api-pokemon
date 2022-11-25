13940756726d9b337ece6c1d1ca84ac16a8334c8
Agustin Romero <agusromero0815@gmail.com>
2022-11-25 13:08:54 -0300

    First Commit

--- src/routes/middlewares/extractUser.js
+++ src/routes/middlewares/extractUser.js
@@ -2,16 +2,20 @@ const bcrypt = require("bcrypt");
 const { User } = require("../../db.js");
 
 module.exports = async (req, res, next) => {
-  const password = await bcrypt.hash(req.body.password, 8);
+  const { email, password } = req.body;
 
   try {
     const user = await User.findOne({
       where: {
-        password,
+        email,
       },
     });
 
     if (!user) throw new Error();
+
+    const verificatePassword = await bcrypt.compare(password, user.password);
+
+    if (!verificatePassword) throw new Error();
   } catch (e) {
     return res.status(401).json({
       error: "No authorization",
